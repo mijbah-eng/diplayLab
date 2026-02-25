828f86db618cd8791427aede363843f412530c60
MijbahMERN <misbhakhandoker@gmail.com>
2026-02-25 11:58:35 +0600

    update navber 2.0

--- src/components/NavberMain.jsx
+++ src/components/NavberMain.jsx
@@ -5,8 +5,25 @@ import { useEffect, useState } from "react";
 
 function NavberMain() {
   const [showNavShadow, setShowNavShadow] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-//   shawNavShadow
+  const toggleHandler = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 991);
+    };
+    setIsOpen(false);
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isMobile]);
+
+  //   shawNavShadow
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 10) {
@@ -23,42 +40,105 @@ function NavberMain() {
       className={`flex flex-col sticky w-full  justify-center bg-white top-0 z-[999] ${showNavShadow ? "scrolled" : ""}`}
     >
       <div className="navShadow !top-0 !h-full  opacity-0 transition-opacity duration-300 ease-in-out pointer-events-none pb-0 flex absolute inset-x-0"></div>
-      <div className="static z-[1000 bg-[#ddd]">
+      <div className="static z-[1000] bg-[#ddd]">
         <div className="bg-white flex flex-col justify-center h-[5rem]">
-          <div className="px-[2.5rem]">
-            <div className="relative flex justify-between items-center w-full max-w-7xl m-auto px-0">
+          <div className="px-[1.25rem] md:px-[1.5rem] lg:px-[2.5rem]">
+            <div className="static lg:relative flex justify-between items-center w-full max-w-7xl m-auto px-0">
               <Link className="w-50" href={"#"}>
                 {" "}
                 <img src={logoBlue} alt="Logo" />
               </Link>
-              <div className="static flex justify-between items-center w-full">
-                <div className="flex w-full justify-center">
-                  <div className="flex items-center mx-auto">
+              {/* nav tab group wrapper  */}
+              <div
+                className={`mobile_nav_tab lg:static flex justify-between items-center w-full transition-all duration-300 ease-in-out transform-gpu [transform-style:preserve-3d] ${isMobile ? (isOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-8 pointer-events-none") : ""}`}
+              >
+                <div className="menu_wrapper flex w-full justify-center">
+                  {/* nav tabs group */}
+                  <div className="menu_main flex items-center mx-auto">
                     {navData.map((singleNav, index) => (
                       <Link
                         key={index}
-                        className="flex items-center py-[.75rem] px-[1.25rem] font-semibold transition-[all .2s] text-[#425466] hover:text-[#333]"
+                        className="nav_link-mm flex items-center py-[.75rem] px-[1.25rem] font-semibold transition-[all .2s] text-[#425466] hover:text-[#333]"
                         href={singleNav?.navLink}
                       >
                         {singleNav?.navTab}
                       </Link>
                     ))}
                   </div>
-                  <div className="flex justify-center items-center gap-4">
+                  {/* nav buttons group */}
+                  <div className=" menu_action flex justify-center items-center gap-4">
                     <Link
-                      className="border-[1.5px] border-[#00be5f] bg-[#00be5f] text-white py-[0.4rem] px-[1.5rem] text-base font-medium leading-[180%] transition-colors duration-200 rounded-sm hover:bg-white hover:text-black"
+                      className="border-[1.5px] border-[#00be5f] bg-[#00be5f] text-white text-center capitalize whitespace-nowrap rounded flex justify-center items-center px-6 py-[.4rem] text-base font-medium leading-[180%] transition-colors duration-200 hover:text-black hover:bg-white"
                       href={"#"}
                     >
                       Log in
                     </Link>
                     <Link
-                      className="border-[1.5px] border-[#105783] bg-[#105783] text-white py-[0.4rem] px-[1.5rem] text-base font-medium leading-[180%] transition-colors duration-200 rounded-sm hover:bg-white hover:text-[#105783]"
+                      className="border-[1.5px] border-[#105783] bg-[#105783] text-white text-center capitalize whitespace-nowrap rounded flex justify-center items-center px-6 py-[.4rem] text-base font-medium leading-[180%] transition-colors duration-200 hover:bg-white hover:text-black"
                       href={"#"}
                     >
                       Try For Free
                     </Link>
                   </div>
                 </div>
+              </div>
+              <div
+                onClick={toggleHandler}
+                className={` ${isMobile ? "" : "hidden"} flex items-center py-6 pl-6 cursor-pointer`}
+              >
+                {/* = */}
+                {isOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <script xmlns="" id="eppiocemhmnlbhjplcgkofciiegomcon" />
+                    <script xmlns="" />
+                    <script xmlns="" />
+                    <path
+                      d="M6 18L18 6M6 6L18 18"
+                      stroke="#717E8A"
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <script xmlns="" id="eppiocemhmnlbhjplcgkofciiegomcon" />
+                    <script xmlns="" />
+                    <script xmlns="" />
+                    <path
+                      d="M2.18164 4.90912H21.818"
+                      stroke="#717E8A"
+                      strokeWidth="3.27273"
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M2.18164 12H21.818"
+                      stroke="#717E8A"
+                      strokeWidth="3.27273"
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M2.18164 19.0909H21.818"
+                      stroke="#717E8A"
+                      strokeWidth="3.27273"
+                      strokeLinecap="round"
+                    />
+                  </svg>
+                )}
+
+                {/* x */}
               </div>
             </div>
           </div>
